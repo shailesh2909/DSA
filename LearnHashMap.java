7746a950145502fd6e1d77905177ecb542f2601f
shailesh2909 <shaileshsuryawanshi443103@gmail.com>
2025-02-05 01:03:59 +0530

    Map

---
+++ LearnHashMap.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class LearnHashMap {
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+        
+        @SuppressWarnings("rawtypes")
+        Map<String, Integer> map = new HashMap();
+
+        map.put("One",1);
+        map.put("Two",2);
+        map.put("Three",3);
+        map.put("Four",4);
+        map.put("Five",5);
+
+        map.putIfAbsent("Two", 23);
+
+        System.out.println(map);
+
+        System.out.println(map.containsKey("Two"));
+        System.out.println(map.containsValue(4));
+
+        for(Map.Entry<String, Integer> e : map.entrySet())
+        {
+            System.out.println(e);
+            System.out.println(e.getKey());
+            System.out.println(e.getValue());   
+        }
+
+        System.out.println(map.get("Three"));
+        map.remove("Three");
+
+        System.out.println(map);
+
+        for(String key : map.keySet())
+        {
+            System.out.println(key);
+        }
+
+        for(Integer value : map.values())
+        {
+            System.out.println(value);
+        }
+    }
+}
