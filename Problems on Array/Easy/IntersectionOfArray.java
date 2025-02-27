e3928c61b4347a8a84e3e08f13f760672c29cb19
shailesh2909 <shaileshsuryawanshi443103@gmail.com>
2025-02-28 02:02:27 +0530

    intersection

--- Problems on Array/Easy/IntersectionOfArray.java
+++ Problems on Array/Easy/IntersectionOfArray.java
@@ -1,41 +1,17 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-//import java.util.TreeSet;
 
-public class UnionOfArray {
-
-    //brute force approach
-    /*public static ArrayList<Integer> findUnion(int a[], int b[]) {
-        
-        TreeSet<Integer> set = new TreeSet<>();
-        
+public class IntersectionOfArray {
+    public static ArrayList<Integer> findIntersection(int a[], int b[])
+    {
         int n1 = a.length;
         int n2 = b.length;
-        
-        for(int i = 0; i<n1; i++)
-        {
-            set.add(a[i]);
-        }
-        
-        for(int i = 0; i<n2; i++)
-        {
-            set.add(b[i]);
-        }
-        
-        return new ArrayList<>(set);
-    }*/
 
-    //better approach
-    public static ArrayList<Integer> findUnion(int a[], int b[]) {
-        
         ArrayList<Integer> list = new ArrayList<>();
-        
-        int n1 = a.length;
-        int n2 = b.length;
-        
+
         int i = 0;
-        int j = 0;
-        
+        int j = 0;  
+
         while(i<n1 && j<n2)
         {
             if(i>0 && a[i]==a[i-1])
@@ -43,21 +19,19 @@ public class UnionOfArray {
                 i++;
                 continue;
             }
-            
+
             if(j>0 && b[j]==b[j-1])
             {
                 j++;
                 continue;
             }
-            
+
             if(a[i]<b[j])
             {
-                list.add(a[i]);
                 i++;
             }
             else if(a[i]>b[j])
             {
-                list.add(b[j]);
                 j++;
             }
             else
@@ -67,26 +41,10 @@ public class UnionOfArray {
                 j++;
             }
         }
-        
-        while(i<n1)
-        {
-            if(i==0 || a[i]!=a[i-1])
-            {
-                list.add(a[i]);
-            }
-            i++;
-        }
-        
-        while(j<n2)
-        {
-            if(j==0 || b[j]!=b[j-1])
-            {
-                list.add(b[j]);
-            }
-            j++;
-        }
-        
+
         return list;
+
+
     }
 
     public static void main(String[] args) {
@@ -107,13 +65,14 @@ public class UnionOfArray {
         {
             b[i] = sc.nextInt();
         }
-        ArrayList<Integer> list = findUnion(a, b);
-        System.out.println("Union of the two arrays:");
+        ArrayList<Integer> list = findIntersection(a, b);
+        System.out.println("Intersection of the two arrays:");
+
         for(int i=0; i<list.size(); i++)
         {
             System.out.print(list.get(i)+" ");
         }
+        
         sc.close();
     }
-    
 }
